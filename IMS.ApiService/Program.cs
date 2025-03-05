5283ef025c15ba013c03419e4ab8437d6ddcc6eb
leonardo alvarado perez <leonardoperez00@hotmail.com>
2025-03-05 13:44:25 -0600

    Agregar archivos de proyecto.

---
+++ IMS.ApiService/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add service defaults & Aspire client integrations.
+builder.AddServiceDefaults();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add services to the container.
+builder.Services.AddProblemDetails();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseExceptionHandler();
+
+app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapDefaultEndpoints();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ Model/Entities/IncidentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entities
+{
+    class IncidentModel
+    {
+        public int Id { get; set; }
+        public required string Category { get; set; }
+        public required string Description { get; set; }
+        public string? Comments { get; set; }
+        public string? SupportUser { get; set; }
+        public required string Status { get; set; }
+    }
+}
